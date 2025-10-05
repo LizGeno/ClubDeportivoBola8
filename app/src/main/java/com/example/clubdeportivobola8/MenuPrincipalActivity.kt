7ba3675b4906292409ee1eff621921b7c899fa9f
Julian Benitez <jibenitez79@gmail.com>
2025-10-04 22:35:28 -0300

    Creación de PagarCuotaActivity y navegación desde el menú principal

--- app/src/main/java/com/example/clubdeportivobola8/MenuPrincipalActivity.kt
+++ app/src/main/java/com/example/clubdeportivobola8/MenuPrincipalActivity.kt
@@ -21,13 +21,10 @@ class MenuPrincipalActivity : AppCompatActivity() {
         // 1. Encontrar el botón para registrar socio por su ID
         val btnSocio = findViewById<AppCompatButton>(R.id.btnSocio)
 
-        // 2. Configurar el OnClickListener
+
         btnSocio.setOnClickListener {
-            // 3. Crear un Intent para iniciar AltaSocioActivity
-            // ASEGÚRATE DE QUE LA CLASE ES UNA ACTIVITY
             val intent = Intent(this, AltaSocioActivity::class.java)
 
-            // 4. Iniciar la nueva actividad
             startActivity(intent)
         }
         val btnPagarCuota = findViewById<AppCompatButton>(R.id.btnPagarCuota)
@@ -37,17 +34,4 @@ class MenuPrincipalActivity : AppCompatActivity() {
         }
     }
 
-    // La función navigateTo para Fragments no es necesaria en esta Activity,
-    // ya que solo lanza otras Activities. Se puede eliminar para evitar confusiones.
-    /*
-    private fun navigateTo(fragment: Fragment, addToBackStack: Boolean = true) {
-        val transaction = supportFragmentManager.beginTransaction()
-            .replace(R.id.main_activity_container, fragment)
-
-        if (addToBackStack) {
-            transaction.addToBackStack(null)
-        }
-        transaction.commit()
-    }
-    */
 }
